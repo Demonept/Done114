04ab3fc74ddb501ef766146ca75d9a7acffcf061
Demonept <66496493+Demonept@users.noreply.github.com>
2022-12-01 16:55:18 +0300

    Созданы методы для работы с базой данных. Так же обновлен класс для подключения к базе данных

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,75 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+//        Util connect = new Util();
+        UserService userS = new UserServiceImpl();
+        UserDao userDao = new UserDaoJDBCImpl();
+//        Connection con = connect.getConnection();
+//        con.close();
+        User user1 = new User("Имя","Фамилия", (byte) 12);
+        User user2 = new User("Дмитрий","Савченко", (byte) 28);
+        User user3 = new User("Василий","Непобежденный", (byte) 44);
+        User user4 = new User("Кирилл","Дверьзапилил", (byte) 24);
+
+
+
+//        userS.createUsersTable();
+
+        userS.saveUser(user1.getName(),user1.getLastName(),user1.getAge());
+        userS.saveUser(user2.getName(),user2.getLastName(),user2.getAge());
+        userS.saveUser(user3.getName(),user3.getLastName(),user3.getAge());
+        userS.saveUser(user4.getName(),user4.getLastName(),user4.getAge());
+        userS.saveUser(user1.getName(),user1.getLastName(),user1.getAge());
+
+
+        System.out.print(userS.getAllUsers().toString());
+//        System.out.println(userS.getAllUsers().
+
+
+//        userS.removeUserById(2);
+//        userS.removeUserById(1);
+//        userS.cleanUsersTable();
+//        userS.removeUserById(1);
+//        userS.removeUserById(1);
+//        userS.dropUsersTable();
+
+
+
+
+//        try(Connection con = new Util().getConnection()){
+//            Statement statement = con.createStatement();
+//            statement.executeUpdate("DROP TABLE new_schema.users");
+//        }catch (SQLException e){
+//            throw e;
+////            System.out.println("Произошла ошибка при удалении базы данных!");
+        }
+
+//        try{
+//            Statement statement = con.createStatement();
+//            ResultSet resultSet = statement.executeQuery(query);
+//            preparedStatement.executeUpdate();
+//            System.out.println("Персона добавлена");
+//            while(resultSet.next()){
+//                String id = resultSet.getString("name");
+//                System.out.println(id);
+//            }
+//        }
+//        catch (SQLException e){
+//            throw e;
+////            System.out.println("Не вышло");
+//        }
+
+//        Statement statement = util.createStatement();
     }
-}
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,38 @@
 package jm.task.core.jdbc.util;
 
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static String DB_URL = "jdbc:mysql://localhost:3306";
+    private static String DB_USER = "root";
+    private static String DB_PASS = "root";
+
+    private Connection connect;
+
+    public Connection getConnection() {
+        connect = null;
+        try {
+            Class.forName(DB_DRIVER);
+            connect = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+            System.out.println("Успешная попытка подключения");
+        } catch (SQLException | RuntimeException | ClassNotFoundException e) {
+            System.out.println("Неудачная попытка подключения");
+            return null;
+        }
+        return connect;
+    }
+//    public void closeConnection(Connection connect) throws SQLException {
+//        try{
+//            connect.close();
+//            System.out.println("Подключение успешно закрыто");
+//        }
+//        catch(SQLException e){
+//            System.out.println("Закрытие подключения выдало ошибку");
+//        }
+//    }
+
 }
